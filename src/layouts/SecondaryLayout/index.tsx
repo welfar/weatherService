0470b0b94320bd0f45ba6383201bd0f11b2e6f39
Welfar <jdwelfar91@gmail.com>
2025-01-02 10:02:50 +0100

    The component is modified by removing functions and simplifying the layout to make it more dynamic

--- src/layouts/SecondaryLayout/index.tsx
+++ src/layouts/SecondaryLayout/index.tsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Sidebar } from "../../components/SideBar";
-import { ContactForm } from "../../features/ContactForm";
 import "./SecondaryLayout.scss";
 
-export const SecondaryLayout: React.FC<React.PropsWithChildren<{}>> = () => {
-  const navigate = useNavigate();
-
-  const handleCitySelect = (city: string) => {
-    console.log(`Selected city: ${city}`);
-  };
-
-  const handleContactClick = () => {
-    console.log(`Navegando`);
-    navigate("/contact");
-  };
-
+export const SecondaryLayout: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
   return (
     <div className="secondary-layout">
-      <Sidebar
-        onSelectCity={handleCitySelect}
-        onContactClick={handleContactClick}
-      />
-      <main className="secondary-layout__content">
-        <ContactForm />
-      </main>
+      <main className="secondary-layout__content">{children}</main>
     </div>
   );
 };
